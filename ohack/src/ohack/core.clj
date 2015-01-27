ebc36612d27ef9489e89a84630e2874262e0110d
natto <natto@natto-mba.local>
2015-01-27 14:08:33 -0500

    add synth demo section

--- ohack/src/ohack/core.clj
+++ ohack/src/ohack/core.clj
@@ -26,3 +26,34 @@
 (odoc odoc)
 
 
+;; so `synth` contains a bunch of instrument definitions that you can
+;; use already. you can try them by calling e.g.
+(synth/ping)
+
+;; some of them will fade away after a while.
+(synth/rise-fall-pad)
+
+;; some of them don't
+(synth/vintage-bass)
+
+;; in which case, stop(), i.e. overtone.live/stop(), switches off all sounds
+(stop)
+
+;; tip: C-M-x evals the expression (to the outermost surrounding
+;; expression!) that your cursor is /inside/, or /before/. To evaluate
+;; the expression that your cursor is /immdiately after/, use C-x-e
+
+;; other synths to try
+(comment
+  (synth/daf-bass)
+  (synth/buzz)
+  (synth/cs80lead)
+  (synth/ticker)
+  (synth/pad)
+  (synth/bass)
+  (synth/daf-bass)
+  (synth/grunge-bass)
+  (synth/ks1)
+
+  (stop)
+  )
