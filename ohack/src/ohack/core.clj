93a723a57b96f95b198bcb5e4110272a1b35d64e
natto <natto@natto-mba.local>
2015-01-27 13:30:05 -0500

    add boot up comment

--- ohack/src/ohack/core.clj
+++ ohack/src/ohack/core.clj
@@ -3,3 +3,11 @@
         [overtone.inst.piano]
         [overtone.inst.sampled-piano])
   (:require [overtone.inst.synth :as synth]))
+
+;; run M-x cider-jack-in to get started
+;;
+;; once started, move your cursor to the (ns ...) block above and hit
+;; C-M-x to evaluate it and boot the supercollider server.  if your
+;; sampled-piano samples are not cached, overtime will attempt to
+;; download them and cache them now. once you see the prompt update with
+;; the Overtone logo, you are good to go.
