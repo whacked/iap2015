be43cb1d605e3a557367e50bc9fac70c4b1d7c95
natto <natto@natto-mba.local>
2015-01-27 21:21:23 -0500

    add play function, and play it

--- ohack/src/ohack/core.clj
+++ ohack/src/ohack/core.clj
@@ -270,29 +270,31 @@
                                         ;; these will be grouped into sets of 6 lines, with indexes
                                         (sort index-group-list))
 
+      play! (partial guitar-pick-note-sequence 80)
       ]
 
   ;; now we apply to everything
-  (apply
-   concat
-   (map (fn [play-map]
-          (let [max-beat (apply max (keys play-map))]
-            (map play-map (range max-beat))))
-        (for [line-set sorted-grouped-line-set-list]
-          ;; map over each line in the line-set, with string index
-          (apply
-           merge-with concat
-           ;; note the reverse
-           (for [[string-index tab-line] (map-indexed vector (reverse line-set))]
-             ;; now we need to parse the tab-line...
-             ;; collect each string's results into into {}
-             (into {}
-                   ;; create index -> []
-                   ;; when no string is played, for rest
-                   (map (fn [[k v]] [k (if v
-                                        [string-index v]
-                                        [])])
-                        (parse-guitar-tab-line tab-line))))))))
+  (play!
+   (apply
+    concat
+    (map (fn [play-map]
+           (let [max-beat (apply max (keys play-map))]
+             (map play-map (range max-beat))))
+         (for [line-set sorted-grouped-line-set-list]
+           ;; map over each line in the line-set, with string index
+           (apply
+            merge-with concat
+            ;; note the reverse
+            (for [[string-index tab-line] (map-indexed vector (reverse line-set))]
+              ;; now we need to parse the tab-line...
+              ;; collect each string's results into into {}
+              (into {}
+                    ;; create index -> []
+                    ;; when no string is played, for rest
+                    (map (fn [[k v]] [k (if v
+                                         [string-index v]
+                                         [])])
+                         (parse-guitar-tab-line tab-line)))))))))
 
 
 )
