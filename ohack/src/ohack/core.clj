5f780a7a5a63aae6aff3d961c074998bcd1336ab
natto <natto@natto-mba.local>
2015-01-29 18:10:08 -0500

    inc

--- ohack/src/ohack/core.clj
+++ ohack/src/ohack/core.clj
@@ -618,7 +618,7 @@
                ;; now it plays a single note with all the instruments
                ((:instrument track-state) note))))
        ;; if there are notes left, we need to schedule play-track-map again
-       (when false
+       (when (some (fn [& x] false) (range 10))
          (apply-at (metro next-tick) play-track-map [next-tick
                                                      ;; we need to send in track-map
                                                      ;; where rest() is applied to each :melody value
